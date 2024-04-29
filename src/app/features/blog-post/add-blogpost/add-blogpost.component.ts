d468efaa151d700fb0b2a02d5f4f5e5b359557b7
jurifauser1395 <jurifauser@pm.me>
2024-04-29 19:54:37 +0200

    implement correct published date input field

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { AddBlogPost } from '../Models/add-blog-post.model';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-blogpost',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, DatePipe],
   templateUrl: './add-blogpost.component.html',
-  styleUrl: './add-blogpost.component.css'
+  styleUrl: './add-blogpost.component.css',
 })
 export class AddBlogpostComponent {
+  model: AddBlogPost;
 
+  constructor() {
+    this.model = {
+      title: '',
+      shortDescription: '',
+      urlHandle: '',
+      content: '',
+      featuredImageUrl: '',
+      author: '',
+      isVisible: true,
+      publishedDate: new Date(),
+    };
+  }
+
+  onFormSubmit(): void {
+    console.log(this.model);
+  }
 }
