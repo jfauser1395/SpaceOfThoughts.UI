11e449fbb8c0f237902501cfd95e7f3a5da306cc
jfauser1395 <juri.fauser@iu-study.org>
2024-06-16 15:21:13 +0200

    adjust angular.json to disable ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { importProvidersFrom, isDevMode } from '@angular/core';
@@ -15,6 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     importProvidersFrom(),
     provideMarkdown(),
+    provideHttpClient(withFetch()),
     provideHttpClient(withInterceptors([authInterceptor])),
     CookieService, provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
