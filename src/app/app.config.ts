90a0eb756a329734eccdc8d10b3546f3b22aa78b
jfauser1395 <juri.fauser@iu-study.org>
2024-06-28 16:43:24 +0200

    change file structure and references

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -20,6 +20,9 @@ export const appConfig: ApplicationConfig = {
     CookieService, provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           }),
   ],
 };
