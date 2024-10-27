21766b993d04ab1cbc96f67794ae3b21ef87d22a
jfauser1395 <juri.fauser@iu-study.org>
2024-10-27 19:23:06 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -11,6 +11,8 @@ sudo npm install -g @angular/cli
 # Download and install MySQL Server
 sudo apt install -y mysql-server
 
+sleep 2
+
 # Setup the project's default database
 sudo mysql < SpaceOfThoughts.UI/InstallationScript/mysql_commands.sql
 
@@ -20,12 +22,17 @@ sudo apt install -y dotnet-sdk-8.0
 # Download and install EF framework Core
 dotnet tool install --global dotnet-ef
 
-# Update PATH for the EF framework
-export PATH="$PATH:/root/.dotnet/tools"
+sleep 2
 
+# Update PATH for the EF framework
+echo 'export PATH="$PATH:/home/$(whoami)/.dotnet/tools"' >> ~/.profile
+source ~/.profile
 
 # Setup the API
 cd SpaceOfThoughts.API/
+
+sleep 2
+
 sudo rm -rf Migrations/*
 dotnet ef migrations add InitialCreate --context ApplicationDbContext
 dotnet ef migrations add InitialCreateAuth --context AuthDbContext
@@ -36,4 +43,4 @@ dotnet linux-dev-certs install
 dotnet run &
 
 # Open a new terminal window to run npm install and ng serve
-gnome-terminal -- bash -c "cd ~/SpaceOfThoughts.UI/ && npm install && ng serve; exec bash"
+gnome-terminal -- bash -c "cd ~/SpaceOfThoughts.UI && npm install && ng serve; exec bash"
